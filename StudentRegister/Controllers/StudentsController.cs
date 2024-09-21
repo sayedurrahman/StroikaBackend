376f0283660abcb89fedd0493463eefb3d106101
sayedurrahman <sayedur.rahmans@gmail.com>
2024-09-22 01:44:50 +0600

    DTO classes are inside folder

--- StudentRegister/Controllers/StudentsController.cs
+++ StudentRegister/Controllers/StudentsController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using StudentRegister.Models;
+using StudentRegister.Models.DTOs;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
