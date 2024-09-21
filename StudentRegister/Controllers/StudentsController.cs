435c1106e2fb4d0e736bd92099863375c3e42596
sayedurrahman <sayedur.rahmans@gmail.com>
2024-09-21 17:25:35 +0600

    Student controller updated

--- StudentRegister/Controllers/StudentsController.cs
+++ StudentRegister/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentRegister.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +9,73 @@ namespace StudentRegister.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
-        // GET: api/<StudentsController>
+        // GET: api/Students
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Student> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new Student[] {
+                new() { ID = 1, FirstName = "John", LastName= "Doe", DateOfBirth = DateTime.Parse("2023-07-31T12:44:55.403Z")},
+                new() { ID = 2, FirstName = "John2", LastName= "Doe2", DateOfBirth = DateTime.Parse("2023-07-31T12:44:55.403Z")}
+            };
         }
 
-        // GET api/<StudentsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET api/Students/5
+        //[HttpGet("{id}")]
+        private Student Get(int id)
         {
-            return "value";
+            return new() { ID = 1, FirstName = "John", LastName = "Doe", DateOfBirth = DateTime.Parse("2023-07-31T12:44:55.403Z") };
         }
 
-        // POST api/<StudentsController>
+        // POST api/Students
+        //TODO: check input
         [HttpPost]
-        public void Post([FromBody] string value)
+        public Student Post([FromBody] string value)
         {
+            return Get(0);
         }
 
-        // PUT api/<StudentsController>/5
+        // PUT api/Students/5
+        //TODO: check input
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public Student Put(int id, [FromBody] string value)
         {
+            return Get(0);
         }
 
-        // DELETE api/<StudentsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET api/Students/5/Nationality
+        [HttpGet("{id}/Nationality")]
+        public CitizenStudent GetCitizenStudent(int id)
         {
+            return new() { ID = 1, FirstName = "John", LastName = "Doe", NationalityId = 1 };
         }
+
+
+        // PUT api/<StudentsController>/5/Nationality/{3}
+        [HttpPut("{id}/Nationality/{nid}")]
+        public CitizenStudent PutNationalityOfStudent(int id, int nid)
+        {
+            return GetCitizenStudent(0);
+        }
+
+        // GET api/Students/5/FamilyMembers
+        [HttpGet("{id}/FamilyMembers")]
+        public FamilyMember[] GetFamilyMembersOfStudent(int id)
+        {
+            return new FamilyMember[] { new() { ID = 1, FirstName = "John", LastName = "Doe", RelationshipId = 1, DateOfBirth = DateTime.Now } };
+        }
+
+        // POST api/Students/5/FamilyMembers
+        //TODO: check input
+        [HttpPost("{id}/FamilyMembers")]
+        public FamilyMember AddFamilyMemberOfStudent([FromBody] string value)
+        {
+            return new() { ID = 1, FirstName = "John", LastName = "Doe", RelationshipId = 1, DateOfBirth = DateTime.Now };
+        }
+
+        //// DELETE api/<StudentsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
